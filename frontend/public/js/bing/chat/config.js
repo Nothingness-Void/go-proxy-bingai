d6ae35448bcbf8e5645f43658d58fa6844a2975d
Nothingness-Void <55913486+Nothingness-Void@users.noreply.github.com>
2023-09-27 19:56:31 +0800

    Update config.js

--- frontend/public/js/bing/chat/config.js
+++ frontend/public/js/bing/chat/config.js
@@ -191,9 +191,9 @@ _w['_sydConvConfig'] = {
   enableMaxTurnsPerConversation: true,
   enableMaxTurnsFromBackend: false,
 // 设置已登录账号的聊天对话次数
-  maxTurnsPerConversation: 30,
+  maxTurnsPerConversation: 100,
 // 设置未登录账号的聊天对话次数
-  maxTurnsPerConversationMuidUser: 10,
+  maxTurnsPerConversationMuidUser: 50,
   maxMessageLength: 4000,
   enableSigninTurnFix: true,
   enableAmberTurnPerConversation: false,
